a2cd8852f8e6c67307ddc11645595205767b82e0
Mehdi Degryse <mehdi.degryse@feedier.com>
2024-03-03 23:50:27 +0100

    first commit

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+import {ref} from "vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+import { router } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: {
+        type: Boolean,
+    },
+    canRegister: {
+        type: Boolean,
+    },
+    laravelVersion: {
+        type: String,
+        required: true,
+    },
+    phpVersion: {
+        type: String,
+        required: true,
+    },
+});
+
+let url = ref('URL');
+let submitHandler = () => {
+    router.post('/url', url)
+}
+console.log('hi')
+</script>
+
+<template>
+    <Head title="Welcome" />
+    <div>Hello world</div>
+    <form   @submit.prevent
+            @submit="submitHandler"
+    >
+     <input
+        v-model="url">
+        <PrimaryButton>
+        Click me
+        </PrimaryButton>
+    </form>
+
+            <div class="flex justify-center mt-16 px-6 sm:items-center sm:justify-between">
+                <div class="text-center text-sm sm:text-start">&nbsp;</div>
+
+                <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-end sm:ms-0">
+                    Laravel v{{ laravelVersion }} (PHP v{{ phpVersion }})
+                </div>
+            </div>
+</template>
+
+<style>
+.bg-dots-darker {
+    background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(0,0,0,0.07)'/%3E%3C/svg%3E");
+}
+@media (prefers-color-scheme: dark) {
+    .dark\:bg-dots-lighter {
+        background-image: url("data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M1.22676 0C1.91374 0 2.45351 0.539773 2.45351 1.22676C2.45351 1.91374 1.91374 2.45351 1.22676 2.45351C0.539773 2.45351 0 1.91374 0 1.22676C0 0.539773 0.539773 0 1.22676 0Z' fill='rgba(255,255,255,0.07)'/%3E%3C/svg%3E");
+    }
+}
+</style>
